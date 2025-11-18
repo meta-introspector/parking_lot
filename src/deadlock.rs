8c956f6f8973831db238fdbc3ac1a27dc6be7781
mike <h@solfunmeme.com>
2025-11-18 17:59:08 +0000

    wip packaging for cargo2nix

--- src/deadlock.rs
+++ src/deadlock.rs
@@ -35,7 +35,7 @@
 
 #[cfg(feature = "deadlock_detection")]
 pub use parking_lot_core::deadlock::check_deadlock;
-pub(crate) use parking_lot_core::deadlock::{acquire_resource, release_resource};
+pub(crate) use parking_lot_core::deadlock;
 
 #[cfg(test)]
 #[cfg(feature = "deadlock_detection")]
